58bfeeef14354541c8f1c2f2bbcca427bec5b77d
JentseV <s113290@ap.be>
2023-11-03 17:27:35 +0100

    add popup

--- Backend/Backend/Controllers/Match.cs
+++ Backend/Backend/Controllers/Match.cs
@@ -13,33 +13,46 @@ namespace Backend.Controllers
         public MetadataDto metaData { get; set; }
         public InfoDto info { get; set; }
 
-        public static async Task<List<string>> GetMatchIDs(string puuid, int count,string API_KEY)
+        public static async Task<List<string>> GetMatchIDs(string puuid, int count, string API_KEY)
         {
-           HttpClient client = new HttpClient();
-           HttpResponseMessage httpResponse = await client.GetAsync($"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={count}&api_key={API_KEY}");
-           Debug.WriteLine(puuid);
-            string responseBody = await httpResponse.Content.ReadAsStringAsync();
-           Debug.WriteLine(responseBody);
-           var matchIDs = JsonConvert.DeserializeObject<List<string>>(responseBody);
-           return matchIDs;
+            try
+            {
+                HttpClient client = new HttpClient();
+                HttpResponseMessage httpResponse = await client.GetAsync($"https://europe.api.riotgames.com/lol/match/v5/matches/by-puuid/{puuid}/ids?start=0&count={count}&api_key={API_KEY}");
+                Debug.WriteLine(puuid);
+                string responseBody = await httpResponse.Content.ReadAsStringAsync();
+                Debug.WriteLine(responseBody);
+                var matchIDs = JsonConvert.DeserializeObject<List<string>>(responseBody);
+                return matchIDs;
+            }
+            catch
+            {
+                Console.WriteLine("fuckup in getMatchIDs");
+                throw new Exception("GetMachIDs() error");
+            }
         }
 
         public static async Task<List<Match>> GetMatches(List<string> matchIDs, string API_KEY)
         {
-            var matches = new List<Match>();
-
-            HttpClient httpClient = new HttpClient();
-
-            
-            foreach(var match in matchIDs)
+            try
             {
-                HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key={API_KEY}");
-                var responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
-                var matchesDetails = JsonConvert.DeserializeObject<Match>(responseBody);
-                matches.Add(matchesDetails);
+                var matches = new List<Match>();
+                HttpClient httpClient = new HttpClient();
+                foreach (var match in matchIDs)
+                {
+                    HttpResponseMessage httpResponseMessage = await httpClient.GetAsync($"https://europe.api.riotgames.com/lol/match/v5/matches/{match}?api_key={API_KEY}");
+                    var responseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+                    var matchesDetails = JsonConvert.DeserializeObject<Match>(responseBody);
+                    matches.Add(matchesDetails);
+                }
+                return matches;
             }
-
-            return matches;
+            catch
+            {
+                Console.WriteLine("Fuckup in get matches");
+                throw new Exception("GetMaches() error");
+                
+            }
         }
     }
 }
